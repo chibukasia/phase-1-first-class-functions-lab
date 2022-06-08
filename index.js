b276a13e023722ce43cb01cb057480f8d55c50d7
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-08 14:24:45 +0300

    commit tree 17fde246f6b50bd3b70fd2d035e0c4c2f8673989

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2)
+    const newDrivers = [...drivers] 
+    return newDrivers.slice(0, 2)
 }
 
 const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2)
+    const newDrivers = [...drivers] 
+    return newDrivers.slice(-2)
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
@@ -20,9 +22,19 @@ const fareDoubler = function (createFareMultiplier) {
 const fareTripler = function(createFareMultiplier) {
     return (createFareMultiplier * 3);
 }
-function selectDifferentDrivers(drivers, selectingDrivers) {
-    const firstDrivers = returnFirstTwoDrivers(drivers)
-    const lastDrivers = returnLastTwoDrivers(drivers)
-    return firstDrivers, lastDrivers;
-
+function selectDifferentDrivers(drivers, fan) {
+    const firstDrivers = (drivers)
+    //const secondDriver = selectingDrivers
+    //const lastDrivers = returnLastTwoDrivers(drivers)
+    if (fan===selectingDrivers[0]){
+        return returnFirstTwoDrivers(drivers)
+      }else{
+        return returnLastTwoDrivers(drivers)
+      }
+    //return drivers[0], selectingDrivers[0]
+    //console.log(selectingDrivers)
+    //return firstDrivers, lastDrivers;
 }
+
+selectDifferentDrivers (returnFirstTwoDrivers)
+//selectDifferentDrivers (returnLastTwoDrivers)
